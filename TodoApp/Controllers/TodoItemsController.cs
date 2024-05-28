9c281a97acc77fa41f3f1c0f8488024d9cec59fa
1orenz <118253614+LorenzVlad@users.noreply.github.com>
2024-05-28 11:24:40 +0300

    Initial commit

---
+++ TodoApp/Controllers/TodoItemsController.cs
@@ -0,0 +1,159 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using TodoApp.Data;
+using TodoApp.Models;
+using TodoApp.Data;
+using TodoApp.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace TodoApp.Controllers
+{
+    [Authorize]
+    public class TodoItemsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public TodoItemsController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: Todo
+        public async Task<IActionResult> Index()
+        {
+            var userId = _userManager.GetUserId(User);
+            var todoItems = await _context.TodoItems.Where(t => t.UserId == userId).ToListAsync();
+            return View(todoItems);
+        }
+
+        // GET: Todo/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var todoItem = await _context.TodoItems.FirstOrDefaultAsync(m => m.Id == id);
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            return View(todoItem);
+        }
+
+        // GET: Todo/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Todo/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Title,Description,DueDate")] TodoItem todoItem)
+        {
+            
+                todoItem.UserId = _userManager.GetUserId(User);
+                _context.Add(todoItem);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            
+            //return View(todoItem);
+        }
+
+        // GET: Todo/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var todoItem = await _context.TodoItems.FindAsync(id);
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+            return View(todoItem);
+        }
+
+        // POST: Todo/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, TodoItem todoItem)
+        {
+            if (id != todoItem.Id)
+            {
+                return NotFound();
+            }
+
+            // Знайти існуючий об'єкт в базі даних за його ідентифікатором
+            var existingTodoItem = await _context.TodoItems.FindAsync(id);
+
+            // Оновити властивості існуючого об'єкту з даними, що прийшли з форми
+            existingTodoItem.Title = todoItem.Title;
+            existingTodoItem.Description = todoItem.Description;
+            existingTodoItem.DueDate = todoItem.DueDate;
+            existingTodoItem.IsCompleted = todoItem.IsCompleted;
+
+            try
+            {
+                // Зберегти зміни в базі даних
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TodoItemExists(todoItem.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Todo/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var todoItem = await _context.TodoItems
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            return View(todoItem);
+        }
+
+        // POST: Todo/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+            _context.TodoItems.Remove(todoItem);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TodoItemExists(int id)
+        {
+            return _context.TodoItems.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ TodoApp/Models/TodoItem.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace TodoApp.Models
+{
+
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool IsCompleted { get; set; }
+        public string UserId { get; set; }
+    }
+}
